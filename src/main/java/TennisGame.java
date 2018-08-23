fe01de7b88fee62ae9307a4df97924c03359587d
franek-326 <mariusz.francuz@gmail.com>
2018-08-23 11:00:06 +0200

    Initial commit

---
+++ src/main/java/TennisGame.java
@@ -0,0 +1,81 @@
+public class TennisGame {
+
+    private int player1Score;
+    private int player2Score;
+
+    public TennisGame() {
+        player1Score = 0;
+        player2Score = 0;
+    }
+
+    public String getScore() {
+
+        String score = transformPlayerScoreNumberToScoreName(player1Score, player2Score);
+
+
+        return score;
+    }
+
+    private String transformPlayerScoreNumberToScoreName(int player1Score, int player2Score) {
+        String player1ScoreName = "Love";
+        String player2ScoreName = "Love";
+
+        if (isAdvantageForPlayer(player1Score, player2Score)) {
+            return "Advantage player1";
+        }
+        if (isAdvantageForPlayer(player2Score, player1Score)) {
+            return "Advantage player2";
+        }
+
+        if (hasPlayerWon(player1Score, player2Score)) {
+            return "Win for player1";
+        }
+        if (hasPlayerWon(player2Score, player1Score)) {
+            return "Win for player2";
+        }
+
+        if (player1Score == 1) {
+            player1ScoreName = "Fifteen";
+        } else if (player1Score == 2) {
+            player1ScoreName = "Thirty";
+        } else if (player1Score == 3) {
+            player1ScoreName = "Forty";
+        }
+
+        if (player2Score == 1) {
+            player2ScoreName = "Fifteen";
+        } else if (player1Score == 2) {
+            player2ScoreName = "Thirty";
+        } else if (player1Score == 3) {
+            player2ScoreName = "Forty";
+        }
+
+        return player1ScoreName + " - " + player2ScoreName;
+    }
+
+    /*
+        checks if player provided in the first argument has advantage over the other player
+        (at least 3 points for both players and
+        player with advantage has one point more than the other player)
+     */
+    private boolean isAdvantageForPlayer(int playerScore, int theOtherPlayerScore) {
+        return theOtherPlayerScore >= 3 && playerScore == theOtherPlayerScore + 1;
+    }
+
+    /*
+        checks if the player provided in the first argument has won
+        (at least 4 points and 2 more than the other player)
+     */
+    private boolean hasPlayerWon(int playerScore, int theOtherPlayerScore) {
+        return playerScore >= 4 && playerScore >= theOtherPlayerScore + 2;
+    }
+
+
+    public void wonPoint(String s) {
+        if (s.equals("player1")) {
+            player1Score++;
+        } else {
+            player2Score++;
+        }
+    }
+}
---
+++ src/test/java/TennisGameTests.java
@@ -0,0 +1,184 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TennisGameTests {
+
+    @Test
+    // 0 - 0 Love - Love
+    public void shouldDisplayLoveLoveOnTheGameStart() {
+        // GIVEN
+        TennisGame tg = new TennisGame();
+        String score = tg.getScore();
+
+        // WHEN THEN
+        assertThat(score).isEqualTo("Love - Love");
+    }
+
+    @Test
+    // 1 - 0 Fifteen - Love
+    public void shouldDisplayFifteenLoveWhenWonPointForPlayerOneIsCalledAfterGameStarts() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player1");
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Fifteen - Love");
+
+
+    }
+
+    @Test
+    // 0 - 1 Love - Fifteen
+    public void shouldDisplayLoveFifteenWhenWonPointForPlayerOneIsCalledAfterGameStarts() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player2");
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Love - Fifteen");
+
+
+    }
+
+    @Test
+    // 1-1 Fifteen - Fifteen
+    public void shouldDisplayFifteenFifteenWhenWonPointForPlayerOneAndTwoAreCalledAfterGameStarts() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player1");
+        tg.wonPoint("player2");
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Fifteen - Fifteen");
+
+
+    }
+
+    @Test
+    // 2-1 Thirty - Fifteen
+    public void shouldDisplayThirtyFifteenWhenTheScoreIs21() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player1");
+        tg.wonPoint("player2");
+        tg.wonPoint("player1");
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Thirty - Fifteen");
+
+    }
+
+
+    @Test
+    // 3-1 - Forty - Fifteen
+    public void shouldDisplayFortyFifteenWhenTheScoreIs41() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player1");
+        tg.wonPoint("player2");
+        tg.wonPoint("player1");
+        tg.wonPoint("player1");
+
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Forty - Fifteen");
+
+    }
+
+    @Test
+    // 4-1 - Win for player1
+    public void shouldDisplayWinForPlayer1WhenTheScoreIs41() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player1");
+        tg.wonPoint("player2");
+        tg.wonPoint("player1");
+        tg.wonPoint("player1");
+        tg.wonPoint("player1");
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Win for player1");
+
+    }
+
+    @Test
+    // 1-4 - Win for player2
+    public void shouldDisplayWinForPlayer2WhenTheScoreIs14() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player2");
+        tg.wonPoint("player1");
+        tg.wonPoint("player2");
+        tg.wonPoint("player2");
+        tg.wonPoint("player2");
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Win for player2");
+
+    }
+
+    @Test
+    // 4-3 - Advantage player1
+    public void shouldDisplayAdvantagePlayer1WhenTheScoreIs43() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player1");
+        tg.wonPoint("player2");
+        tg.wonPoint("player2");
+        tg.wonPoint("player2");
+        tg.wonPoint("player1");
+        tg.wonPoint("player1");
+        tg.wonPoint("player1");
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Advantage player1");
+
+    }
+
+    @Test
+    // 3-4 - Advantage player2
+    public void shouldDisplayAdvantagePlayer2WhenTheScoreIs34() {
+        //GIVEN
+        TennisGame tg = new TennisGame();
+
+        //WHEN
+        tg.wonPoint("player2");
+        tg.wonPoint("player1");
+        tg.wonPoint("player1");
+        tg.wonPoint("player1");
+        tg.wonPoint("player2");
+        tg.wonPoint("player2");
+        tg.wonPoint("player2");
+        String score = tg.getScore();
+
+        //THEN
+        assertThat(score).isEqualTo("Advantage player2");
+
+    }
+
+}
